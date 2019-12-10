7140be7ccff8e10cf52b094b37a6ef1ec4d9581d
Tran Ly <tranly237@gmail.com>
2019-12-11 00:02:26 +0700

    populate

--- app/controllers/teacher.controller.js
+++ app/controllers/teacher.controller.js
@@ -1,25 +1,31 @@
 const Teacher = require('../models/teacher.model');
 const User = require('../models/user.model');
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const ObjectId = require('mongodb').ObjectID
+
 
 /**
  * Get info of teacher
  * body: { id}
  */
 exports.getInfo = async (req, res) => {
-    // const id = req.body
-    // console.log("id: ", id)
+    const _id = req.body
+    // console.log("id: ", _id)
 
-    // try {
-    //     const objId = mongoose.mongo.BSONPure.ObjectID.fromHexString(id);
-    //     console.log("id: ", objId)
-    //     const result = await Teacher.findById(id)
-    //     // .populate('userId', { passwordHash: 0, password: 0 })
-    // console.log("id: ")
+    try {
+        const result = await Teacher.findOne({ userId: _id }).populate('userId', { passwordHash: 0, password: 0 })
+        // console.log("id: ", result)
+        if (!result) {
+        return res.status(400).send({ message: 'Không tìm thấy thông tin người dùng!'});
+        }
 
-    //     return res.status(200).send({ payload: result });
-    // } catch (err){
-    //     console.log("err: ", err);
-    //     return res.status(500).send({ message: 'Đã có lỗi xảy ra, vui lòng thử lại!'});
-    // }
+        const { city, district, ward } = studentInfo;
+        const userResult = studentInfo.userId;
+
+
+        return res.status(200).send({ payload: result });
+    } catch (err){
+        console.log("err: ", err);
+        return res.status(500).send({ message: 'Đã có lỗi xảy ra, vui lòng thử lại!'});
+    }
 };
