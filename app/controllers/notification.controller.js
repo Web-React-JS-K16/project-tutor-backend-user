eb9216b65ec8c3e1d48898c2d77d73ea3697b6e8
Trần Khánh Linh <trankhanhlinh98@gmail.com>
2019-12-20 00:05:04 +0700

    add update is read, is deleted for notification

--- app/controllers/notification.controller.js
+++ app/controllers/notification.controller.js
@@ -9,9 +9,15 @@ exports.getNotificationList = async (req, res) => {
   var itemPerPage = req.query.limit || DefaultValues.itemPerPage;
 
   try {
-    const result = await Notification.find({ userId: ObjectId(userId) });
+    const result = await Notification.find({
+      userId: ObjectId(userId),
+      isDeleted: false
+    }).sort({
+      createdAt: -1
+    });
     return res.status(200).send({ payload: result });
-  } catch {
+  } catch (err) {
+    console.log('error: ', err.message);
     return res
       .status(500)
       .send({ message: 'Đã có lỗi xảy ra, vui lòng thử lại!' });
@@ -23,10 +29,58 @@ exports.countNotifications = async (req, res) => {
 
   try {
     const result = await Notification.countDocuments({
-      userId: ObjectId(userId)
+      userId: ObjectId(userId),
+      isDeleted: false
     });
     return res.status(200).send({ payload: result });
-  } catch {
+  } catch (err) {
+    console.log('error: ', err.message);
+    return res
+      .status(500)
+      .send({ message: 'Đã có lỗi xảy ra, vui lòng thử lại!' });
+  }
+};
+
+exports.updateIsReadNotification = async (req, res) => {
+  var id = req.params.id;
+
+  try {
+    if (id) {
+      const result = await Notification.updateOne(
+        { _id: ObjectId(id) },
+        { $set: { isRead: true } }
+      );
+      return res
+        .status(200)
+        .send({ message: 'Cập nhật thông tin thành công.' });
+    } else {
+      return res.status(400).send({ message: 'Mã thông báo không tồn tại.' });
+    }
+  } catch (err) {
+    console.log('error: ', err.message);
+    return res
+      .status(500)
+      .send({ message: 'Đã có lỗi xảy ra, vui lòng thử lại!' });
+  }
+};
+
+exports.updateIsDeletedNotification = async (req, res) => {
+  var id = req.params.id;
+
+  try {
+    if (id) {
+      const result = await Notification.updateOne(
+        { _id: ObjectId(id) },
+        { $set: { isDeleted: true } }
+      );
+      return res
+        .status(200)
+        .send({ message: 'Cập nhật thông tin thành công.' });
+    } else {
+      return res.status(400).send({ message: 'Mã thông báo không tồn tại.' });
+    }
+  } catch (err) {
+    console.log('error: ', err.message);
     return res
       .status(500)
       .send({ message: 'Đã có lỗi xảy ra, vui lòng thử lại!' });
